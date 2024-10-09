d835c24d7491f7244e3fa61beb47e35c8972f9c6
bhimaniparag123 <159759757+bhimaniparag123@users.noreply.github.com>
2024-10-09 16:10:22 +0530

    Add files via upload

---
+++ 9-10.js
@@ -0,0 +1,92 @@
+
+
+//#1
+
+// let i;
+// for(i=10; i>=1; i--){
+// 	console.log(i);
+// }
+// console.log("outside loop: ====> ",i);	// 6
+
+// --------------------------------
+
+
+
+//#2
+// let number = Number(prompt('Enter number: '));
+
+
+//  for(let i = 51; i<=number; i++){		// i = i + 1
+// 	if(i % 2 == 1){
+// 		console.log(i);
+// 	}	
+// }
+
+
+// for (let i = 1; i <= number; i += 2) {		// i = i + 2
+// 	console.log(i);
+// }
+
+// ----------------------------
+
+
+
+//#3
+
+// let n = parseInt(prompt("Enter a number:"));
+
+// for (let i = 1; i <= n; i++) {
+//     console.log(i);
+// }
+
+// ========================
+
+//#4
+
+// let n = parseInt(prompt("Enter a number:"));
+
+// for (let i = n; i >= 1; i--){
+// 	console.log(i);
+	
+// }
+
+//======================
+
+
+
+//#5
+
+//let n= parseInt(prompt("Enter a number:"));
+
+// function printRange() {
+//     for (let i = -n; i <= n; i++) {
+//         console.log(i);
+//     }
+// } 
+// printRange(n);
+
+//====================
+
+
+//#6
+
+// let n= parseInt(prompt("Enter a number:"));
+// function printOddNumbers(n) {
+//     for (let i = 1; i <= n; i += 2) {
+//         console.log(i);
+//     }
+// } 
+// printOddNumbers(n);
+
+// ============================
+
+
+//#7
+// let n= parseInt(prompt("Enter a number:"));
+
+// function printEvenNumbers(n) {
+//     for (let i = 0; i <= n; i += 2) {
+//         console.log(i);
+//     }
+// }
+// printEvenNumbers(n);
